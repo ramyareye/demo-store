b01ba567aa176d3d4057dd365bb8ff28d7ccaa08
Reza Babaei <ramyariran@gmail.com>
2023-01-05 00:23:12 +0700

    feat: force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(ShippingModifiers $shippingModifiers)
     {
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
+        
         $shippingModifiers->add(
             ShippingModifier::class
         );
